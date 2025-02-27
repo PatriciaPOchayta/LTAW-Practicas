30ca94cc1bffb349140c2574864b6ffb27456c21
patriciapochayta <p.perezo.2019>
2025-02-27 12:07:02 +0100

    Eliminar carpetas

--- P1/S2/Ej-03.js
+++
@@ -1,9 +0,0 @@
-//-- Ejemplo de bucles
-
-//-- Definiendo una constante: Número de mensajes
-const N = 10; 
-
-//-- Bucle para imprimir N mensajes
-for (let i = 0; i < N; i++) {
-    console.log("Mensaje " + i);
-}
