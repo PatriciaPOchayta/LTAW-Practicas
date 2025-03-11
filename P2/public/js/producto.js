2be27d5ee1d62c2363518eea0e008d7917e6b560
patriciapochayta <p.perezo.2019>
2025-03-11 19:11:42 +0100

    crear js de los productos

---
+++ P2/public/js/producto.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Producto - The Lucky Dog</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <h1>THE LUCKY DOG</h1>
+
+  <div class="product-container">
+    <img id="producto-imagen" src="" alt="Producto">
+    
+    <div class="product-info">
+      <h2 id="producto-nombre"></h2>
+      <p id="producto-descripcion"></p>
+      <p class="price" id="producto-precio"></p>
+      <button id="add-to-cart">Añadir al carrito</button>
+    </div>
+  </div>
+
+  <a href="index.html" class="back-link">⬅ Volver a la tienda</a>
+
+  <script src="js/producto.js"></script>
+</body>
+</html>
